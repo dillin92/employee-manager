e9254c6ac3afee3c7939442244aed067ed4133dd
dillin92 <dillin970@gmail.com>
2021-08-02 18:47:49 -0400

    initial database setup

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXIST employee_management_system;
+CREATE DATABASE employee_management_system;
+USE employee_management_system;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees (first_name,last_name, role_id, manager_id)
+VALUES 
+('Dillin', 'Helsley', 1, 2),
+('Kelsi', 'Helsley',1, 0),
+('Kris', 'Tyler', 2, 1), 
+('Taylor', 'Gossage', 2, 1),
+('James', 'Oakes', 3, 4),
+('Nathan','Shepherd',3, 4),
+('Riece', 'Hover', 3, 5),
+('Cayden', 'Biggs', 4, 4),
+('Ezrith', 'Biggs', 4, 5);
